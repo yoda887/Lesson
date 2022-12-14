ac9d83f692af14f7ab35652764fd2134b87cbf22
BogdanM <dedust1980@gmail.com>
2022-12-15 00:36:42 +0200

    HH

---
+++ src/Plan.java
@@ -0,0 +1,38 @@
+public class Plan {
+
+    /*
+
+    [] BASIC SYNTAX
+    [] DATA TYPES
+    [] VARIABLES
+    [] CONSTANTS
+    [] CONDITIONALS
+    [] FUNCTIONS
+    [] LOOPS
+    [] EXCEPTION HANDLING
+    [] DATA STRUCTURES
+    [] OOP
+    [] INTERFACES
+    [] CLASSES
+    [] METHODS
+    [] POINTERS
+    [] PACKAGES
+    [] COLLECTION FRAMEWORK
+    [] STREAMS
+    [] GENERICS
+    [] ARRAY
+    [] ARRAY LIST
+    [] SERIALIZATION
+    [] MEMORY MANAGEMENT
+    [] ROUTINES
+    [] THREADS
+    [] WORKING WITH FILES & SOCKETS
+    [] GARBAGE COLLECTION
+    [] HOW JVM WORKS?
+
+
+    **/
+
+
+
+}
